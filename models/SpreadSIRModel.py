6600ba5082e152a509037058bd9016495edabc9e
adozaraz <minecraft-no.adozaraz@yandex.ru>
2022-12-07 21:04:02 +0400

    Изменение концепции SpreadSIRModel

--- models/SpreadSIRModel.py
+++ models/SpreadSIRModel.py
@@ -2,28 +2,38 @@ import numpy as np
 
 
 class SpreadSIRModel:
-    def __init__(self, Time, nt, Area, stepArea, population, beta, gamma, Ds, Di, Dr):
-        self.Time = Time
-        self.ht = Time / nt
-        self.Area = Area
-        self.hx = tuple(map(lambda x, nx: x / nx, zip(Area, stepArea)))
+    def __init__(self, Time, nt, LengthX, nx, population, beta, gamma, Ds, Di, Dr, LengthY=None, ny=None,
+                 LengthZ=None, nz=None):
+        # Model related variables
         self.population = population
         self.beta = beta
         self.gamma = gamma
+        # Area and time related variables
+        self.ht = Time / nt
+        self.hx = LengthX / nx
+        self.x = np.linspace(-LengthX, LengthX, int(2 * LengthX / self.hx) + 1)
+        self.t = np.linspace(0, Time, int(Time / self.ht) + 1)
+        self.shape = [self.t.shape[0], self.x.shape[0]]
+        if not (LengthY is None and ny is None):
+            self.hy = LengthY / ny
+            self.y = np.linspace(-LengthY, LengthY, int(2 * LengthY / self.hy) + 1)
+            self.shape.append(self.y.shape[0])
+            if not (LengthZ is None and nz is None):
+                self.hz = LengthZ / nz
+                self.z = np.linspace(-LengthZ, LengthZ, int(2 * LengthZ / self.hz) + 1)
+                self.shape.append(self.z.shape[0])
+        # Dispersion related variables
         self.Ds = Ds
         self.Di = Di
         self.Dr = Dr
-        self.x = np.linspace(-Length, Length, int(2 * Length / self.hx) + 1)
-        self.t = np.linspace(0, Time, int(Time / self.ht) + 1)
-        self.S = np.zeros((self.x.shape[0], self.t.shape[0]))
-        self.I = np.zeros((self.x.shape[0], self.t.shape[0]))
-        self.K = np.zeros((self.x.shape[0], self.t.shape[0]))
+        # SIR Model variables
+        self.S = np.zeros(self.shape)
+        self.I = np.zeros(self.shape)
+        self.K = np.zeros(self.shape)
 
     def runSimulation(self):
-        for i in range(self.t.shape[0] + 1):
-            pass
+        pass
 
     def spreadPeopleRandomly(self):
-        randomPeople = np.random.dirichlet(np.ones(self.x.shape[0] - 2), size=1)
-        self.S[0] = self.population * randomPeople
-        self.S[0][0] =
+        randomPeople = np.random.dirichlet(np.ones(self.shape[1:]), size=1)
+        pass
